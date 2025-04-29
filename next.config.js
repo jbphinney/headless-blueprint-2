38c1bb39799e95e95337ca3298773e805462fdaf
jbphinney <jbphinney@gmail.com>
2025-04-29 12:38:31 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,19 +5,6 @@ const { withFaust, getWpHostname } = require('@faustwp/core');
  **/
 module.exports = withFaust({
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=600, s-maxage=86400, stale-while-revalidate=86400',
-          },
-        ],
-      },
-    ]
-  },
   sassOptions: {
     includePaths: ['node_modules'],
   },
